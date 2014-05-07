86f920f5eb23ebaa3cbd30d9e96ed80c5f39042b
kingmfs14 <kingm@fullsail.edu>
2014-05-07 13:43:39 -1000

    follow along with data objects video

---
+++ data-objects/main.py
@@ -0,0 +1,37 @@
+
+import webapp2
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        
+        luke = Character()
+        luke.name = 'Luke Skywalker'
+        luke.profession = 'Jedi Knight'
+        luke.age = 26
+        luke.home_planet = 'Tattoine'
+
+        leia = Character()
+        leia.name = 'Princess Leia'
+        leia.profession = 'Princess'
+        leia.age = luke.age
+        leia.home_planet = 'Alderan'
+
+        yoda = Character()
+        yoda.name = 'Master Yoda'
+        yoda.profession = 'Jedi Knight'
+        yoda.age = 762
+        yoda.home_planet = 'Dagobah'
+
+        chars = [luke, leia, yoda]
+        print chars[1].profession
+
+class Character(object):
+	def __init__(self): #constructor function
+		self.name = ''
+		self.profession = ''
+		self.age = 0
+		self.home_planet = ''
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
