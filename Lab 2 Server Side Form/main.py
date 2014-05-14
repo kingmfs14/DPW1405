6b7fa9bea74cd9ed1789d61e56be17c8cfcd6527
kingmfs14 <kingm@fullsail.edu>
2014-05-13 21:10:50 -1000

    Server-side-form TEXT INPUT

--- Lab 2 Server Side Form/main.py
+++ Lab 2 Server Side Form/main.py
@@ -16,6 +16,19 @@ class MainHandler(webapp2.RequestHandler):
 	</head>
 	<body>'''	
 
+	page_form = '''<h1>Enter Your Profile Information Below</h1>
+		<form method="GET">
+			<p>
+				<label>First Name: </label><input type="text" name="f_name" /> <br>
+				<label>Last Name: </label><input type="text" name="l_name" />
+			</p>
+			<p>
+				<label>Email: </label><input type="text" name="email" /> <br>
+				<label>Phone: </label><input type="text" name="phone" />
+			</p>
+			<input type="submit" value="Submit" />
+		</form>'''
+
 	page_close = '''</body>
 </html>
         '''
