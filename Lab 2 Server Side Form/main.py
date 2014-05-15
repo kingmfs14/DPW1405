4a04f64559bf337f778899f8ef5ac183c300e824
kingmfs14 <kingm@fullsail.edu>
2014-05-14 19:00:45 -1000

    Server-side-form ADDED TITLE CHANGE BETWEEN PAGES

--- Lab 2 Server Side Form/main.py
+++ Lab 2 Server Side Form/main.py
@@ -12,7 +12,7 @@ class MainHandler(webapp2.RequestHandler):
     	page_head = '''<!DOCTYPE HTML>
 <html>
 	<head>
-		<title>Profile Information Form</title>
+		<title>{title}</title>
 		<link href="css/style.css" rel="stylesheet" type="text/css" />
 	</head>
 	<body>'''	
@@ -20,27 +20,30 @@ class MainHandler(webapp2.RequestHandler):
 	page_form = '''<h1>Enter Your Profile Information Below</h1>
 		<form method="GET">
 			<p>
-				<label>First Name: </label><input type="text" name="f_name" /> <br>
-				<label>Last Name: </label><input type="text" name="l_name" />
+				<label>First Name: </label><input type="text" name="f_name" style="width:290px;" />
+				<label>Last Name: </label><input type="text" name="l_name" style="width:290px;" />
 			</p>
 			<p>
-				<label>Email: </label><input type="text" name="email" /> <br>
-				<label>Phone: </label><input type="text" name="phone" />
+				<label>Email: </label><input type="text" name="email" style="width:290px;" />
+				<label>Phone: </label><input type="text" name="phone" style="width:290px;" />
 			</p>
 			<p>
 				<label>Contact by: <label>
-				<select name="contact">
-					<option value="phone"> Phone </option>
-					<option value="email"> Email </option>
-				</select>
+					<select name="contact">
+						<option value="phone"> Phone </option>
+						<option value="email"> Email </option>
+					</select>
 			</p>
 			<p>
 				<label>Expertise: </label><br>
-				<input type="checkbox" name="expertise" value="HTML5" />HTML5 
-				<input type="checkbox" name="expertise" value="CSS3" />CSS3 
-				<input type="checkbox" name="expertise" value="JavaScript" />JavaScript 
-				<input type="checkbox" name="expertise" value="PHP" />PHP 
-				<input type="checkbox" name="expertise" value="Python" />Python 
+					<input type="checkbox" name="expertise" value="HTML5" />HTML5 
+					<input type="checkbox" name="expertise" value="CSS3" />CSS3 
+					<input type="checkbox" name="expertise" value="JavaScript" />JavaScript 
+					<input type="checkbox" name="expertise" value="PHP" />PHP 
+					<input type="checkbox" name="expertise" value="Python" />Python 
+			</p>
+			<p>
+				<label>Additional Information: </label><textarea name="info" style="width:390px;"></textarea>
 			</p>
 			<input type="submit" value="Submit" />
 		</form>'''
@@ -71,6 +74,10 @@ class MainHandler(webapp2.RequestHandler):
 				<th>Expert In</th>
 				<td>{expertise}<td>
 			</tr>
+			<tr>
+				<th>Additional Information</th>
+				<td>{info}<td>
+			</tr>
 		</table>
 	'''
 
@@ -79,15 +86,20 @@ class MainHandler(webapp2.RequestHandler):
         '''
 
         if self.request.GET:
+        	title = 'Your Profile Information'
         	f_name = self.request.GET['f_name']
         	l_name = self.request.GET['l_name']
         	email = self.request.GET['email']
         	phone = self.request.GET['phone']
         	contact = self.request.GET['contact']
         	expertise = self.request.GET.getall('expertise')
+        	info = self.request.GET['info']
         	page_info = page_info.format(**locals())
+        	page_head = page_head.format(**locals())
         	self.response.write(page_head + page_info + page_close)
         else:
+        	title = 'Profile Information Form'
+        	page_head = page_head.format(**locals())
         	self.response.write(page_head + page_form + page_close)
 
 app = webapp2.WSGIApplication([
