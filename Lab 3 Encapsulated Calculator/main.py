73cbd47eb0d7e4e8cf4814e2d72f20ae3608e7c9
kingmfs14 <kingm@fullsail.edu>
2014-05-14 23:55:41 -1000

    Encapsulation-calculator CREATED B DATA

--- Lab 3 Encapsulated Calculator/main.py
+++ Lab 3 Encapsulated Calculator/main.py
@@ -21,6 +21,17 @@ class MainHandler(webapp2.RequestHandler):
     	a.calc_cost()
     	print self.response.write("Allen's Loan will have a payment of " + str(a.payment) + " a month. He will pay a total of " + str(a.total_cost) + " including " + str(a.interest) + " in interest total.")
 
+    	#Bob's Loan Information
+    	b = Loan()
+    	b.name = 'Bob'
+    	b.amount = 7000
+    	b.years = 2.25
+    	b.interest_rate = 6.8
+    	b.calc_payment()
+    	b.calc_interest()
+    	b.calc_cost()
+    	print self.response.write("<br> Bob's Loan will have a payment of " + str(b.payment) + " a month. He will pay a total of " + str(b.total_cost) + " including " + str(b.interest) + " in interest total.")
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
