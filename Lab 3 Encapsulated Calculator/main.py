6f8637b06b35c75bfcfe1a8e06223e9182e4e967
kingmfs14 <kingm@fullsail.edu>
2014-05-14 23:54:31 -1000

    Encapsulation-calculator CREATED A DATA

--- Lab 3 Encapsulated Calculator/main.py
+++ Lab 3 Encapsulated Calculator/main.py
@@ -10,6 +10,16 @@ from loans import Loan
 
 class MainHandler(webapp2.RequestHandler):
     def get(self): 
+    	#Allen's Loan Information
+    	a = Loan()
+    	a.name = 'Allen'
+    	a.amount = 6000
+    	a.years = 10
+    	a.interest_rate = 6.8
+    	a.calc_payment()
+    	a.calc_interest()
+    	a.calc_cost()
+    	print self.response.write("Allen's Loan will have a payment of " + str(a.payment) + " a month. He will pay a total of " + str(a.total_cost) + " including " + str(a.interest) + " in interest total.")
 
 
 app = webapp2.WSGIApplication([
