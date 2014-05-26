d9b13dc27a58bf0125802d37bf36ecdf9acdf520
kingmfs14 <kingm@fullsail.edu>
2014-05-26 09:39:17 -1000

    Fox-say ADDED ANIMAL ABSTRACT CLASS

--- Lab 4 What Does the Fox Say/main.py
+++ Lab 4 What Does the Fox Say/main.py
@@ -8,8 +8,21 @@ Lab 4 What Does the Fox Say
 import webapp2
 
 class MainHandler(webapp2.RequestHandler):
-    def get(self):
+    def get(self): #function that starts everything
     	pass
+   	
+class Animal(object): #Animal Abstract class for various other Amazon animals 
+	def __init__(self): 
+		self.name = ''
+		self.phylum = ''
+		self.clss = ''
+		self.order = ''
+		self.family = ''
+		self.genus = ''
+		self.url = ''
+		self.avg = ''
+		self.habitat = ''
+		self.geoloc = ''
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
