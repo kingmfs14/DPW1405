a3f8579779e20295c5a1c8a9497e24ec3564fc45
kingmfs14 <kingm@fullsail.edu>
2014-05-28 20:37:44 -1000

    Itunes-app CREATED MULTIPLE SEARCH ENGINE OPTION

--- itunes-app/main.py
+++ itunes-app/main.py
@@ -5,18 +5,13 @@ import json
 
 class MainHandler(webapp2.RequestHandler):
 	def get(self):
-		p = FormPage()
-		p.inputs = [['term', 'text', 'Song Name or Artist'],['album', 'artist', 'song'],['Submit', 'submit']]
+		p = Page()
 		self.response.write(p.print_out())
+		f = FormPage()
+		f.inputs = [['term', 'text', 'Song Name or Artist'],['Submit', 'submit']]
 
 		if self.request.GET:
-			#get info from API
-			term = 'term='
-			term += self.request.GET['term']
-			term = term.replace(' ', '+')
-			entity = '&entity='
-			attr = '&attribute='
-			limit = '&limit=200'
+			self.response.write(f.print_out())
 			if self.request.GET['search'] is 'album':
 				entity += 'album'
 				attr += 'albumTerm'
@@ -26,6 +21,13 @@ class MainHandler(webapp2.RequestHandler):
 			else:
 				entity += 'allTrack'
 				attr += 'allTrackTerm'
+			#get info from API
+			term = 'term='
+			term += self.request.GET['term']
+			term = term.replace(' ', '+')
+			entity = '&entity='
+			attr = '&attribute='
+			limit = '&limit=200'
 			url = 'https://itunes.apple.com/search?' + term + entity + attr + limit
 			#assemble the request
 			request = urllib2.Request(url)
@@ -33,8 +35,6 @@ class MainHandler(webapp2.RequestHandler):
 			opener = urllib2.build_opener()
 			#use the url to get a result - request info from API
 			result = opener.open(request)
-
-			print result
 
 			#parsing with JSON
 			jsondoc = json.load(result)
@@ -51,11 +51,16 @@ class Page(object): #borrowing stuff from the object class ABSTRACT CLASS
 		self._head = '''<!DOCTYPE HTML>
 <html>
 	<head>
-		<title></title>
+		<title>iTunes Search Application</title>
 	</head>
 	<body>'''
-
-		self._body = 'Itunes Search'
+		#create a navigation to specify search request type
+		self._body = '''<h1>What Do You Want to Search For?</h1>
+		<ul>
+			<li><a href='search="album"'>An Album</a></li>
+			<li><a href='search="artist"'>An Artist</a></li>
+			<li><a href='search="song"'>A Song</a></li>
+		</ul>'''
 		self._close = '''</body>
 </html>'''	
 	
@@ -79,11 +84,15 @@ class FormPage(Page):
 		#change my private inputs variable
 		self.__inputs = arr
 		#adding text input
-		self._body += '<input type="' + arr[0][1] + '" name="' + arr[0][0] + '" placeholder="' + arr[0][2] + '" /> <br>'
-		#adding select option for user
-		self._body += '<select name="search"><option value="' + arr[1][0] + '">'+arr[1][0]+'</option><option value="' + arr[1][1] + '">'+arr[1][1]+'</option><option value="' + arr[1][2] + '">'+arr[1][2]+'</option></select> <br>'
-		#adding submit button
-		self._body += '<input type="' + arr[2][1] + '" name="' + arr[2][0] + '" /> <br>'
+		for item in arr:
+			self._body += '<input type="' + item[1] + '" name="' + item[0]
+			#if there is a third item... add it in...
+			try:
+				self._body += '" placeholder="' + item[2] + '" /> <br>'
+			#otherwise... end the tag
+			except:
+				self._body += '" /> <br>'
+
 		self._body += '</form>' #ending of form
 
 app = webapp2.WSGIApplication([
