ba9bcfe3eb64abb5d11aa315e06238d2e5c99af4
kingmfs14 <kingm@fullsail.edu>
2014-05-28 19:37:43 -1000

    Itunes-app FIXED SEARCH BUG

--- itunes-app/main.py
+++ itunes-app/main.py
@@ -4,32 +4,36 @@ import urllib2 #python classes and code needed to request info, receive, and ope
 import json
 
 class MainHandler(webapp2.RequestHandler):
-    def get(self):
-    	p = FormPage()
-    	p.inputs = [['term', 'text', 'Song Name or Artist'],['Submit', 'submit']]
-        self.response.write(p.print_out())
+	def get(self):
+		p = FormPage()
+		p.inputs = [['term', 'text', 'Song Name or Artist'],['Submit', 'submit']]
+		self.response.write(p.print_out())
 
-        if self.request.GET:
-	        #get info from API
-	        term = self.request.GET['term']
-	        limit = '&limit=200'
-	        url = 'https://itunes.apple.com/search?' + term + limit
-	        #assemble the request
-	        request = urllib2.Request(url)
-	        #use urllib2 to create and object to get the url
-	        opener = urllib2.build_opener()
-	        #use the url to get a result - request info from API
-	        result = opener.open(request)
+		if self.request.GET:
+			#get info from API
+			term = 'term='
+			term += self.request.GET['term']
+			term = term.replace(' ', '+')
+			limit = '&limit=200'
+			url = 'https://itunes.apple.com/search?' + term + limit
+			#assemble the request
+			request = urllib2.Request(url)
+			#use urllib2 to create and object to get the url
+			opener = urllib2.build_opener()
+			#use the url to get a result - request info from API
+			result = opener.open(request)
 
-	        #parsing with JSON
-	        jsondoc = json.load(result)
-	        print jsondoc
+			print result
 
-	        results = jsondoc['results']
-	        name = results[0]['artistName']
-	        cd = results[0]['collectionName']
+			#parsing with JSON
+			# jsondoc = json.load(result)
+			# print jsondoc
 
-	        self.response.write("You're Artist Name: "+name+'<br> Their first album: '+cd)
+			# results = jsondoc['results']
+			# name = results[0]['artistName']
+			# cd = results[0]['collectionName']
+
+			# self.response.write("You're Artist Name: "+name+'<br> Their first album: '+cd)
 
 class Page(object): #borrowing stuff from the object class ABSTRACT CLASS
 	def __init__(self): #constructor
@@ -74,8 +78,6 @@ class FormPage(Page):
 			#otherwise... end the tag
 			except:
 				self._form_inputs += '" /> <br>'
-
-		print self._form_inputs
 
 	#POLYMORPHISM ALERT!!!-----METHOD OVERRIDE
 	def print_out(self):
