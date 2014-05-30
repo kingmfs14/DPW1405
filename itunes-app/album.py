d209aa4a6f58bc0dffb84d7dbb07e457413c8678
kingmfs14 <kingm@fullsail.edu>
2014-05-29 20:43:04 -1000

    Itunes-app CREATED ALBUM FILE

---
+++ itunes-app/album.py
@@ -0,0 +1,7 @@
+
+class ItunesData(object):
+	"""this data object holds the data fetched by the model and shown by the view"""
+	def __init__(self):
+		self.artist = ''
+		self.album = ''
+		self.cover = ''
